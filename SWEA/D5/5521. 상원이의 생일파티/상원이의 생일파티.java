d923624b165fa611ba5c5b7ed0aaa1842feee1a5
jiwon <128473259+zziwonCHOI@users.noreply.github.com>
2025-05-20 13:11:59 +0900

    [D5] Title: 상원이의 생일파티, Time: 142 ms, Memory: 43,136 KB -BaekjoonHub

--- SWEA/D5/5521. 상원이의 생일파티/상원이의 생일파티.java
+++ SWEA/D5/5521. 상원이의 생일파티/상원이의 생일파티.java
@@ -7,64 +7,69 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Solution {
-    static int n,m;
-    static ArrayList<ArrayList<Integer>> list;
-    static Queue<int[]> search;
-    static int result;
-    static boolean[] visited;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+	static int n, m;
+	static int[][] friend;
+	static int cnt;
+	static boolean[] visited;
 
-        int T=Integer.parseInt(br.readLine());
-        for(int t=1; t<=T; t++){
-            st=new StringTokenizer(br.readLine());
-            n=Integer.parseInt(st.nextToken());
-            m=Integer.parseInt(st.nextToken());
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
 
-            list=new ArrayList<>();
-            for(int i=0; i<=n; i++){
-                list.add(new ArrayList<>());
-            }
+		int T = Integer.parseInt(br.readLine());
+		for (int t = 1; t <= T; t++) {
+			st = new StringTokenizer(br.readLine());
+			n = Integer.parseInt(st.nextToken());
+			m = Integer.parseInt(st.nextToken());
+			friend = new int[n + 1][n + 1];
+			for (int i = 0; i < m; i++) {
+				st = new StringTokenizer(br.readLine());
+				int a = Integer.parseInt(st.nextToken());
+				int b = Integer.parseInt(st.nextToken());
+				friend[a][b] = 1;
+				friend[b][a] = 1;
 
-            search=new LinkedList<>();
-            visited=new boolean[n+1];
-            visited[1]=true;
-            for(int i=0; i<m; i++){
-                st=new StringTokenizer(br.readLine());
-                int a=Integer.parseInt(st.nextToken());
-                int b=Integer.parseInt(st.nextToken());
-                list.get(a).add(b);
-                list.get(b).add(a);
+			}
 
-            }
-            for(int friend:list.get(1)){
-                search.add(new int[]{friend,1});
-                visited[friend]=true;
-            }
+			ArrayList<Integer> list = new ArrayList<Integer>();
+			for (int i = 1; i <= n; i++) {
+				if (friend[1][i] != 0) {
+					list.add(i);
+				}
+			}
+			cnt = 0;
+			visited = new boolean[n + 1];
+			bfs(list);
+			System.out.println("#" + t + " " + cnt);
+		}
+	}
 
-           result=0;
-            bfs();
-            System.out.println("#"+t+" "+result);
-        }
-    }
+	public static void bfs(ArrayList<Integer> l) {
+		visited[1] = true;
+		Queue<int[]> q = new LinkedList<>();
+		for (int i : l) {
+			q.add(new int[] { i, 1 });
+			visited[i] = true;
+			cnt++;
+		}
 
-    public static void bfs(){
-        while(!search.isEmpty()){
-            int[] cur=search.poll();
-            int num=cur[0];
-            int depth=cur[1];
-            result++;
-            if(depth==2) continue;
+		while (!q.isEmpty()) {
+			int[] cur = q.poll();
+			int cNum = cur[0];
+			int d = cur[1];
 
+			if (d == 2)
+				continue;
 
-            for(int next:  list.get(num)){
-               if(!visited[next]){
-                   visited[next]=true;
-                   search.add(new int[]{next,depth+1});
-               }
-            }
+			for (int i = 1; i <= n; i++) {
+				if (!visited[i] && friend[cNum][i] == 1) {
+					visited[i] = true;
+					q.add(new int[] { i, d + 1 });
+					cnt++;
+				}
+			}
 
-        }
-    }
+		}
+
+	}
 }
