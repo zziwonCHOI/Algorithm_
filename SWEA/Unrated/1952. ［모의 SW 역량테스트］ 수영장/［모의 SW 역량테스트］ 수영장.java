2d81782676cb8073187bc4fa05b40e06948d6814
jiwon <128473259+zziwonCHOI@users.noreply.github.com>
2025-05-20 15:53:01 +0900

    [Unrated] Title: [모의 SW 역량테스트] 수영장, Time: 94 ms, Memory: 27,520 KB -BaekjoonHub

--- SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.java
+++ SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.java
@@ -4,44 +4,48 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Solution {
-	static int[] price;
-	static int[] schedule;
-	static int min;
+	static int[] prices;
+	static int[] plan;
+	static int minPrice;
+
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-		
-		int T=Integer.parseInt(br.readLine());
-		for(int t=1; t<=T; t++) {
-			price=new int[4];
-			st=new StringTokenizer(br.readLine());
-			for(int i=0;i<4; i++) {
-				price[i]=Integer.parseInt(st.nextToken());
+
+		int T = Integer.parseInt(br.readLine());
+		for (int t = 1; t <= T; t++) {
+			prices = new int[4];
+			plan = new int[12];
+
+			st = new StringTokenizer(br.readLine());
+			for (int i = 0; i < 4; i++) {
+				prices[i] = Integer.parseInt(st.nextToken());
 			}
-			schedule=new int[12];
-			st=new StringTokenizer(br.readLine());
-			for(int i=0; i<12; i++) {
-				schedule[i]=Integer.parseInt(st.nextToken());
+			st = new StringTokenizer(br.readLine());
+
+			for (int i = 0; i < 12; i++) {
+				plan[i] = Integer.parseInt(st.nextToken());
 			}
-			min=price[3];
-			calCost(0,0);
-			System.out.println("#"+t+" "+min);
-				
+
+			minPrice = prices[3];
+			calPrice(0, 0);
+			System.out.println("#" + t + " " + minPrice);
 		}
 	}
-	private static void calCost(int month, int sum) {
-		if(month>=12) {
-			min=Math.min(min, sum);
+
+	private static void calPrice(int sum, int month) {
+		if (month >= 12) {
+			minPrice = Math.min(minPrice, sum);
 			return;
 		}
-		
-		if(schedule[month]==0) {
-			calCost(month+1, sum);
-		}else {
-			calCost(month+1, sum+schedule[month]*price[0]);
-			calCost(month+1, sum+price[1]);
-			calCost(month+3, sum+price[2]);
+
+		if (plan[month] == 0) {
+			calPrice(sum, month + 1);
+		} else {
+			calPrice(sum + prices[0] * plan[month], month + 1);
+			calPrice(sum + prices[1], month + 1);
+			calPrice(sum + prices[2], month + 3);
 		}
-		
 	}
+
 }
