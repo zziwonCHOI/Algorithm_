00fedf27c97c7155e51c61aca9344242c33fc2b3
jiwon <128473259+zziwonCHOI@users.noreply.github.com>
2025-05-20 11:25:16 +0900

    [Unrated] Title: [모의 SW 역량테스트] 요리사, Time: 293 ms, Memory: 27,796 KB -BaekjoonHub

--- SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.java
+++ SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.java
@@ -1,64 +1,68 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Solution {
-    static int n;
-    static int r;
-    static int[][] arr;
-    static int result;
-    static boolean[] isSelected;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));    
-        StringTokenizer st;
+	static int n;
+	static int[][] taste;
+	static int[] num;
+	static boolean[] selected;
+	static int minDiff;
+	public static void main(String[] args) throws NumberFormatException, IOException {
 
-        int T=Integer.parseInt(br.readLine());
-        for(int t=1; t<=T; t++){
-            n=Integer.parseInt(br.readLine());
-            arr=new int[n][n];
-            for(int i=0; i<n; i++){
-                st=new StringTokenizer(br.readLine());
-                for(int j=0; j<n; j++){
-                    arr[i][j]=Integer.parseInt(st.nextToken());
-                }
-            }
-            r=n/2;
-            result=Integer.MAX_VALUE;
-            isSelected=new boolean[n];
-            comb(0,0);
-            System.out.println("#"+t+" "+result);
-        }
-    }
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
 
-    public static void comb(int idx, int cnt){
-        //n/2만큼 뽑았을 때
-        if(cnt==r){
-            result=Math.min(result,cal());
-            return;
-        }
+		int T = Integer.parseInt(br.readLine());
+		for (int t = 1; t <= T; t++) {
+			n = Integer.parseInt(br.readLine());
+			taste = new int[n][n];
+			for (int i = 0; i < n; i++) {
+				st = new StringTokenizer(br.readLine());
+				for (int j = 0; j < n; j++) {
+					taste[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
 
-        for(int k=idx;k<n;k++){
-            isSelected[k]=true;
-            comb(k+1, cnt+1);
-            isSelected[k]=false;
-        }
-    }
+			num = new int[n / 2];
+			selected = new boolean[n];
+			minDiff=Integer.MAX_VALUE;
+			comb(0, 0);
+			System.out.println("#"+t+" "+minDiff);
+		}
+	}
 
-    public static int cal(){
-        int x=0;
-        int y=0;
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                if(i==j) continue;
+	private static void comb(int cnt, int start) {
+		if (cnt == n / 2) {
+			calTaste();
+			return;
+		}
 
-                if(isSelected[i]&&isSelected[j]){
-                    x+=arr[i][j];
-                }else if(!isSelected[i]&&!isSelected[j]){
-                    y+=arr[i][j];
-                }
-            }
-        }
-        return Math.abs(x-y);
-    }
+		for (int i = start; i < n; i++) {
+			if (selected[i])
+				continue;
+			selected[i] = true;
+			num[cnt] = i;
+			comb(cnt + 1, i + 1);
+			selected[i] = false;
+		}
+	}
+
+	public static void calTaste() {
+		int aSum=0;
+		int bSum=0;
+		for(int i=0; i<n;i++) {
+			for(int j=0; j<n; j++) {
+				if(i==j) continue;
+				if(selected[i]&&selected[j]) {
+					aSum+=taste[i][j];
+				}else if(!selected[i]&&!selected[j]) {
+					bSum+=taste[i][j];
+				}
+			}
+		}
+		minDiff=Math.min(minDiff, Math.abs(aSum-bSum));
+	}
 }
