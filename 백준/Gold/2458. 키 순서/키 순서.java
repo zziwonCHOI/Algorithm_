5222fa519edcaf25e4b9cba7f93b494d71be5726
jiwon <128473259+zziwonCHOI@users.noreply.github.com>
2025-05-20 23:08:37 +0900

    [Gold IV] Title: 키 순서, Time: 456 ms, Memory: 36068 KB -BaekjoonHub

--- 백준/Gold/2458. 키 순서/키 순서.java
+++ 백준/Gold/2458. 키 순서/키 순서.java
@@ -4,43 +4,47 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
-		boolean[][] check = new boolean[n][n];
-		for(int i=0; i<m; i++) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken())-1;
-			int b = Integer.parseInt(st.nextToken())-1;
-			check[a][b] = true;
-		}
-		
-		for(int k=0; k<n; k++) {
-			for(int i=0; i<n; i++) {
-				for(int j=0; j<n; j++) {
-					if(check[i][k] && check[k][j]) {
-						check[i][j] = true;
-					}
-				}
-			}
-		}
-		
-		int[] cnt = new int[n];
-		for(int i=0; i<n; i++) {
-			for(int j=0; j<n; j++) {
-                if(check[i][j] || check[j][i]) {
-					cnt[i] ++;
-				}
-			}
-		}
-		
-		int res =0;
-		for(int num : cnt) {
-			if(num == n-1) res++;
-		}
-		System.out.println(res);
-		
-	}
+    static int n,m;
+    static int[][] stu;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st=new StringTokenizer(br.readLine());
+        n=Integer.parseInt(st.nextToken());
+        m=Integer.parseInt(st.nextToken());
+
+        stu=new int[n+1][n+1];
+        for(int i=0; i<m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            stu[a][b]=1;
+        }
+
+        //모든 학생 쌍에 대해 간접적인 키 비교 결과까지 파악해야 하기 때문에 플로이드 워셜을 사용해야한다.
+        for(int k=1; k<=n; k++){
+            for(int i=1; i<=n; i++){
+                for(int j=1; j<=n; j++){
+                    if(stu[i][k]==1&&stu[k][j]==1){
+                        stu[i][j]=1;
+                    }
+                }
+            }
+        }
+        int cnt=0;
+        for(int i=1; i<=n; i++){
+            int c=0;
+            for(int j=1; j<=n; j++){
+                if(i==j) continue;
+                if(stu[i][j]==1||stu[j][i]==1){
+                    c++;
+                }
+            }
+            if(c==n-1){
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
+    }
 }
