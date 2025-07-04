502b9316f8a2568bbd2cffebdbd38c77b2e052be
jiwon <128473259+zziwonCHOI@users.noreply.github.com>
2025-07-05 00:45:46 +0900

    [Gold IV] Title: 좋다, Time: 120 ms, Memory: 12716 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.java
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
+
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
