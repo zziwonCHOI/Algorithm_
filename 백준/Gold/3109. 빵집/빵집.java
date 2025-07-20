d4c085e33d8b74804ed4b5ef35efd063854a191f
jiwon <128473259+zziwonCHOI@users.noreply.github.com>
2025-07-20 21:28:58 +0900

    [Gold II] Title: 빵집, Time: 300 ms, Memory: 41768 KB -BaekjoonHub

--- 백준/Gold/3109. 빵집/빵집.java
+++ 백준/Gold/3109. 빵집/빵집.java
@@ -1,7 +1,8 @@
 import java.io.BufferedReader;
-import java.io.IOException;
+import java.io.IOException; 
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.StringTokenizer;  
+
 
 public class Main {
 
