290f17a16ee468b493b0676c1dcedce8872d477a
jiwon <128473259+zziwonCHOI@users.noreply.github.com>
2025-10-19 20:54:51 +0900

    [level 3] Title: 여행경로, Time: 0.22 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.js
+++ 프로그래머스/3/43164. 여행경로/여행경로.js
@@ -1,21 +1,21 @@
 function solution(tickets) {
     var answer = [];
     
-    let map=new Map();
+    let m=new Map();
     //그래프 생성 
     for(let [from,to] of tickets){
-        if(!map.has(from)) map.set(from, []);
-        map.get(from).push(to);
+        if(!m.has(from)) m.set(from, []);
+        m.get(from).push(to);
     }
     
     //알파벳 순 정렬
-    for(let [from, tos] of map){
-        map.set(from,tos.sort())
+    for(let [from, tos] of m){
+        m.set(from,tos.sort())
     }
     
     const path=[];
     function dfs(cur){
-        const dest=map.get(cur)||[];
+        const dest=m.get(cur)||[];
         
         while(dest.length>0){
             const next=dest.shift(); //알파벳 순으로 pop
