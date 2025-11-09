11dd63ca59840f21dfb5d250e1cee8214c694c15
jiwon <128473259+zziwonCHOI@users.noreply.github.com>
2025-11-09 19:15:00 +0900

    [level 3] Title: 야근 지수, Time: 123.10 ms, Memory: 70.9 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.java
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.java
@@ -1,7 +1,7 @@
 import java.util.*;
 class Solution {
     public long solution(int n, int[] works) {
-        long answer = 0;
+        long result = 0;
         PriorityQueue<Integer> pq=new PriorityQueue<>(Collections.reverseOrder());
         
         for(int i=0; i<works.length; i++){
@@ -18,8 +18,8 @@ class Solution {
         }
         
         for(int num:pq){
-            answer+=(long)(num*num);
+            result+=(long)(num*num);
         }
-        return answer;
+        return result;
     }
 }
