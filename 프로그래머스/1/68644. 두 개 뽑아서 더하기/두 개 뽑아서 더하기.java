1b8cc5d6404bfc7b24228dda923e115c7e246441
jiwon <128473259+zziwonCHOI@users.noreply.github.com>
2026-02-10 13:47:33 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 2.49 ms, Memory: 88.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] numbers) {
+        int n=numbers.length;
+        
+        Set<Integer> set=new HashSet<>();
+        for(int i=0; i<n; i++){
+            for(int j=i+1;j<n; j++){
+                set.add(numbers[i]+numbers[j]);
+            }
+        }
+        int[] answer = new int[set.size()];
+        int i=0;
+        for(int s:set){
+            answer[i++]=s;
+        }
+        Arrays.sort(answer);
+        return answer;
+    }
+}
