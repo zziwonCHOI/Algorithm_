74d5e1a8f7079fa3ba4570c9e7832104908c1af0
jiwon <128473259+zziwonCHOI@users.noreply.github.com>
2026-02-11 22:47:47 +0900

    [level 2] Title: 피보나치 수, Time: 2.72 ms, Memory: 92 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -1,14 +1,12 @@
 class Solution {
     public int solution(int n) {
-        int answer = 0;
         int[] arr=new int[n+1];
         arr[0]=0;
         arr[1]=1;
         
         for(int i=2; i<n+1; i++){
-            arr[i]=(arr[i-2]+arr[i-1])%1234567;
+            arr[i]=(arr[i-1]+arr[i-2])%1234567;
         }
-        answer=arr[n];
-        return answer;
+        return arr[n];
     }
 }
