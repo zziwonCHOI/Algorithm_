7b025f4914415d8b63369b5783a0f58982cd0ada
jiwon <128473259+zziwonCHOI@users.noreply.github.com>
2026-02-12 00:27:59 +0900

    [level 2] Title: 타겟 넘버, Time: 7.01 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    static int answer=0;
+    public int solution(int[] numbers, int target) {
+        dfs(numbers, target, 0,0);
+        return answer;
+    }
+    
+    public static void dfs(int[] numbers, int target, int depth, int sum){
+        if(depth==numbers.length){
+            if(sum==target){
+                answer++;
+            }
+            return;
+        }
+        
+        dfs(numbers, target, depth+1, sum+numbers[depth]);
+        dfs(numbers, target, depth+1, sum-numbers[depth]);
+    }
+}
