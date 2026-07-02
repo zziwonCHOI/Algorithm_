c55e020a609ffd34dd1ebda692777e7b54e47c1a
jiwon <128473259+zziwonCHOI@users.noreply.github.com>
2026-07-02 15:44:21 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 49.50 ms, Memory: 122 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -3,22 +3,25 @@ import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
-        Map<String, Integer> map=new HashMap<>();
-        int n=participant.length;
         
-        for(int i=0 ;i<n; i++){
-            map.put(participant[i],map.getOrDefault(participant[i],0)+1);
+        Map<String, Integer> players=new HashMap<String, Integer>();
+        
+        for(int i=0; i<participant.length; i++){
+            String name=participant[i];
+            players.put(name,players.getOrDefault(name,0)+1);
         }
         
-        for(int i=0 ;i<completion.length; i++){
-            String cur=completion[i];
-            map.put(cur,map.get(cur)-1);
-        }
-        
-        for(String s:map.keySet()){
-            if(map.get(s)>=1){
-                answer=s;
+        for(int i=0; i<completion.length; i++){
+            String completeName=completion[i];
+            if(players.get(completeName)>1){
+                players.put(completeName,players.get(completeName)-1);
+            }else{
+                players.remove(completeName);
             }
+        }
+        
+        for(String p:players.keySet()){
+            answer=p;
         }
         return answer;
     }
