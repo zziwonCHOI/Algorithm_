be92a586484c701a95b1b7d7d7ed52427c7e12f5
jiwon <128473259+zziwonCHOI@users.noreply.github.com>
2026-07-03 03:16:11 +0900

    [level 1] Title: 달리기 경주, Time: 275.02 ms, Memory: 367 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/README.md
+++ 프로그래머스/1/178871. 달리기 경주/README.md
@@ -1,10 +1,10 @@
 # [level 1] 달리기 경주 - 178871 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/178871?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/178871) 
 
 ### 성능 요약
 
-메모리: 387 MB, 시간: 377.96 ms
+메모리: 367 MB, 시간: 275.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 12월 19일 00:05:59
+2026년 07월 03일 03:16:08
 
 ### 문제 설명
 
--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
@@ -2,29 +2,30 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String[] players, String[] callings) {
-
-        Map<String, Integer> map = new HashMap<>();
-
-        for (int i = 0; i < players.length; i++) {
-            map.put(players[i], i);
+        
+        Map<String,Integer> playerMap=new HashMap<>();
+        Map<Integer,String> rankMap=new HashMap<>();
+        
+        for(int i=0; i<players.length; i++){
+            playerMap.put(players[i],i+1);
+            rankMap.put(i+1,players[i]);
         }
-
-        for (String name : callings) {
-            int idx = map.get(name);
-
-            // 바로 앞 선수와 교환
-            int prevIdx = idx - 1;
-            String front = players[prevIdx];
-
-            // players 배열 교환
-            players[prevIdx] = name;
-            players[idx] = front;
-
-            // map 업데이트
-            map.put(name, prevIdx);
-            map.put(front, idx);
+        
+        for(int i=0; i<callings.length; i++){
+            String callName=callings[i];
+            int curRank=playerMap.get(callName);
+            String beforePlayerName=rankMap.get(curRank-1);
+            playerMap.put(callName,curRank-1);
+            playerMap.put(beforePlayerName,curRank);
+            rankMap.put(curRank,beforePlayerName);
+            rankMap.put(curRank-1,callName);
         }
-
-        return players;
+        
+        String[] answer=new String[players.length];
+        for(int i=0; i<players.length; i++){
+            answer[i]=rankMap.get(i+1);
+        }
+        
+        return answer;
     }
 }
