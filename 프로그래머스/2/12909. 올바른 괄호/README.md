72df45e90d367fdf144614e781c047c343911643
jiwon <128473259+zziwonCHOI@users.noreply.github.com>
2026-07-04 01:17:48 +0900

    [level 2] Title: 올바른 괄호, Time: 13.64 ms, Memory: 66.6 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/README.md
+++ 프로그래머스/2/12909. 올바른 괄호/README.md
@@ -1,10 +1,10 @@
 # [level 2] 올바른 괄호 - 12909 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909) 
 
 ### 성능 요약
 
-메모리: 56.5 MB, 시간: 18.44 ms
+메모리: 66.6 MB, 시간: 13.64 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 02월 11일 01:02:29
+2026년 07월 04일 01:17:44
 
 ### 문제 설명
 
--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -3,25 +3,36 @@ import java.util.*;
 class Solution {
     boolean solution(String s) {
         boolean answer = true;
+
+        Stack<Character> stack=new Stack<>();
+        char[] arr=s.toCharArray();
         
-        Queue<Character> q=new LinkedList<>();
-        char[] c=s.toCharArray();
-        for(int i=0; i<s.length(); i++){
-            if(c[i]=='('){
-                q.add(c[i]);
-            }else{
-                if(q.isEmpty()){
+        for(int i=0; i<arr.length; i++){
+            char cur=arr[i];
+            if(stack.isEmpty()) {
+                if(cur==')'){
                     answer=false;
                     break;
+                }
+                stack.push(arr[i]);
+                continue;
+            }
+            
+            if(cur=='('){
+                stack.push(cur);
+            }else{
+               if(stack.peek()=='('){
+                    stack.pop();
                 }else{
-                    q.poll();
+                    answer=false;
+                    break;
                 }
             }
         }
-        if(!q.isEmpty()){
+
+        if(!stack.isEmpty()){
             answer=false;
         }
-        
         return answer;
     }
 }
