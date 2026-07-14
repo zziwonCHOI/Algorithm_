a6634bb88d8097e0f5bd5efba7b8556b7b0e10f5
jiwon <128473259+zziwonCHOI@users.noreply.github.com>
2026-07-14 20:20:38 +0900

    [level 3] Title: 네트워크, Time: 0.15 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,24 +1,27 @@
-class Solution {
+import java.util.*;
 
+class Solution {
     static boolean[] visited;
     public int solution(int n, int[][] computers) {
         int answer = 0;
         visited=new boolean[n];
+        
         for(int i=0; i<n; i++){
-            if(!visited[i]){
-                dfs(i,n, computers);
-                answer+=1;
-            }
+           if(!visited[i]){
+               dfs(i,computers,n);
+               answer++;
+           }
         }
         return answer;
     }
     
-    public static void dfs(int node, int n, int[][] computers){
-        visited[node]=true;
+    public static void dfs(int curi,int[][] computers,int n){
+    
+        visited[curi]=true;
         
-        for(int i=0;i<n; i++){
-            if(!visited[i]&&computers[node][i]==1){
-                dfs(i,n,computers);
+        for(int i=0; i<n; i++){
+            if(!visited[i]&&computers[curi][i]==1){
+                dfs(i,computers,n);
             }
         }
     }
